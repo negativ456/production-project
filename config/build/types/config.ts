239705ca16f64f851ed6bcd1f5e83b77a300a8be
Daniel <negativ_2002@mail.ru>
2023-01-31 23:36:10 +0300

    initial commit

---
+++ config/build/types/config.ts
@@ -0,0 +1,17 @@
+export type BuildType = "production" | "development"
+
+export interface BuildPaths {
+  entry: string;
+  build: string;
+  html: string;
+}
+export interface BuildEnv {
+  mode: BuildType;
+  port: number;
+}
+export interface BuildOptions {
+  mode: BuildType;
+  paths: BuildPaths;
+  isDev: boolean;
+  port: number;
+}
---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,8 @@
+type Mods = Record<string, boolean | string>
+export function classNames(cls: string, mods: Mods, additional: string[]): string {
+  return [
+      cls,
+      ...additional,
+      ...Object.keys(mods).filter((key) => mods[key])
+  ].join(" ")
+}
