6da792164f2e9a8369dd27ee841aa0a98ca465ae
Daniel <negativ_2002@mail.ru>
2023-06-25 16:32:15 +0300

    feat: netlify config update: api interceptors

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,7 +3,9 @@ import { USER_LOCALSTORAGE_KEY } from '@/shared/const/localstorageKeys';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? '',
-  },
+});
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) ?? '';
+  }
 });
